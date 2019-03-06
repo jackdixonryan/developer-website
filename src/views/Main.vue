a68120cda1c5189419efdf6b12a1ea2f66c676eb
Jack Ryan <jack.dixon.ryan@gmail.com>
2019-03-06 08:58:50 -0700

    Added component, now to work on the slider system.

--- src/views/Main.vue
+++ src/views/Main.vue
@@ -1,9 +1,37 @@
 <template>
   <div>
-    <main-blurb />
-    <main-contact />
-    <main-projects />
-    <main-writings />
+    <button 
+      id="arrow-up"
+      @click="active='main-blurb'"
+    >^</button>
+    <button 
+      id="arrow-right"
+      @click="active='main-contact'"
+    >></button>
+    <button 
+      id="arrow-down"
+      @click="active='main-projects'"
+    >v</button>
+    <button 
+      id="arrow-left"
+      @click="active='main-writings'"
+    ><</button>
+    <main-blurb 
+      v-if="active==='main-blurb'"
+      class="main-component"
+    />
+    <main-contact
+      v-if="active==='main-contact'"
+      class="main-component"
+    />
+    <main-projects 
+      v-if="active==='main-projects'"
+      class="main-component"
+    />
+    <main-writings 
+      v-if="active==='main-writings'"
+      class="main-component"
+    />
   </div>
 </template>
 
@@ -25,12 +53,50 @@ export default {
       active: null,
     }
   },
-  mounted() {
-    console.log("Hello.")
+  methods: {
   }
 }
 </script>
 
-<style>
+<style scoped>
+
+  #arrow-up {
+    position: absolute;
+    left: 50%;
+  }
+
+  #arrow-right {
+    position: absolute;
+    left: 95%;
+    top: 45%;
+  }
+
+  #arrow-down {
+    position: absolute;
+    left: 50%;
+    top: 95%;
+  }
+
+  #arrow-left {
+    position: absolute;
+    top: 45%;
+  }
+
+  button {
+    cursor: pointer;
+    background: none;
+    outline: none;
+    border: none;
+    color: purple;
+    font-size: 1.4em;
+  }
+
+  .main-component {
+    position: absolute;
+    top: 40%;
+    left: 50%;
+    border: 1px purple solid;
+    padding: 1em;
+  }
 
 </style>
